65f5513da6d44a1d239e636843e951d7d2ac1e97
Riio <mnoble25@hotmail.com>
2021-05-26 11:26:32 -0600

    updating js

--- tables/js/script.js
+++ tables/js/script.js
@@ -3,16 +3,16 @@ $(document).ready(function (){
     let anchor = $('a');
     anchor.attr('target', '_blank');
 
-    chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-          if (request.contentScriptQuery == "queryPrice") {
-            var url = "https://www.youtube.com/watch?v=uo35R9zQsAI";
-            fetch(url)
-                .then(response => response.text())
-                .then(text => parsePrice(text))
-                .then(price => sendResponse(price))
-                .catch(error => 'did not work')
-            return true;  
-          }
-        });
+    // chrome.runtime.onMessage.addListener(
+    //     function(request, sender, sendResponse) {
+    //       if (request.contentScriptQuery == "queryPrice") {
+    //         var url = "https://www.youtube.com/watch?v=uo35R9zQsAI";
+    //         fetch(url)
+    //             .then(response => response.text())
+    //             .then(text => parsePrice(text))
+    //             .then(price => sendResponse(price))
+    //             .catch(error => 'did not work')
+    //         return true;  
+    //       }
+    //     });
 })
